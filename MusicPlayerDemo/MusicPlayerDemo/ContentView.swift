413c5608ebc09e6b52d8821f15ca7a11b556f6bf
tisfeng <tisfeng@gmail.com>
2024-11-21 18:24:19 +0800

    feat: add play, backward and forward button

--- MusicPlayerDemo/MusicPlayerDemo/ContentView.swift
+++ MusicPlayerDemo/MusicPlayerDemo/ContentView.swift
@@ -34,35 +34,58 @@ struct ContentView: View {
             }
 
             VStack(spacing: 20) {
-                Image(systemName: isPlaying ? "play.circle" : "pause.circle")
-                    .font(.system(size: 40))
-                    .onReceive(selectedPlayer!.$currentTrack) {
-                        if let track = $0 {
-                            musicTrack = track
-
-                            if let coverImage = $0?.artwork {
-                                artwork = Image(nsImage: coverImage)
-                            } else {
-                                artwork = Image(systemName: "music.note")
-                            }
-                        }
-                    }
-                    .onReceive(selectedPlayer!.$playbackState) {
-                        time = $0.time
-                        isPlaying = $0.isPlaying
-                    }
-
                 Text(musicTrack?.title ?? "")
                     .font(.title)
                 Text((musicTrack?.artist ?? "") + " - " + (musicTrack?.album ?? ""))
 
                 Text("duration: \(String(format: "%.1f", musicTrack?.duration ?? 0))")
                 Text("start time: \(time, specifier: "%.1f")")
+
+                HStack(spacing: 30) {
+                    Button(action: {
+                        selectedPlayer?.skipToPreviousItem()
+                    }) {
+                        Image(systemName: "backward.end")
+                            .font(.system(size: 25))
+                    }
+                    .buttonStyle(.plain)
+
+                    Button(action: {
+                        selectedPlayer?.playPause()
+                    }) {
+                        Image(systemName: isPlaying ? "play.circle" : "pause.circle")
+                            .font(.system(size: 30))
+                    }
+                    .buttonStyle(.plain)
+
+                    Button(action: {
+                        selectedPlayer?.skipToNextItem()
+                    }) {
+                        Image(systemName: "forward.end")
+                            .font(.system(size: 25))
+                    }
+                    .buttonStyle(.plain)
+                }
             }
         }
         .frame(minWidth: 300, minHeight: 300)
         .padding()
         .environment(\.colorScheme, .dark)
+        .onReceive(selectedPlayer!.$currentTrack) {
+            if let track = $0 {
+                musicTrack = track
+
+                if let coverImage = $0?.artwork {
+                    artwork = Image(nsImage: coverImage)
+                } else {
+                    artwork = Image(systemName: "music.note")
+                }
+            }
+        }
+        .onReceive(selectedPlayer!.$playbackState) {
+            time = $0.time
+            isPlaying = $0.isPlaying
+        }
     }
 }
 
